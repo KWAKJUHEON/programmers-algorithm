d75e76c8cb5c78048f846e39c88abb15d26774a2
kwakjuheon <juhun2005@naver.com>
2020-09-22 14:36:40 +0900

    kakaoKeypad

--- src/test/algorithm/programmers/Test.java
+++ src/test/algorithm/programmers/Test.java
@@ -2,13 +2,22 @@ package test.algorithm.programmers;
 
 import test.algorithm.programmers.solution.Solution;
 
+/**
+ * 
+ * @author kwakjuheon
+ * @since Sep 18, 2020
+ *
+ */
 public class Test {
 
+	/**
+	 * 
+	 * @author kwakjuheon
+	 * @since Sep 18, 2020
+	 * @param args
+	 */
     public static void main(String[] args) {
         Solution solution = new Solution();
         solution.runLevel1();
     }
-    
-    
-
 }
--- src/test/algorithm/programmers/solution/level1/KakaoKeypad.java
+++ src/test/algorithm/programmers/solution/level1/KakaoKeypad.java
@@ -1,11 +1,14 @@
 package test.algorithm.programmers.solution.level1;
 
+import java.util.ArrayList;
+
 public class KakaoKeypad {
     // 1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5
     // L R L L L R L L R R L
 
     public void run() {
         System.out.println("************************"); // LRLLLRLLRRL
+        System.out.println("1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5");
         System.out.println(solution(new int[] { 1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5 }, "right"));
         System.out.println("************************"); // LRLLRRLLLRR
         System.out.println(solution(new int[] { 7, 0, 8, 2, 8, 3, 1, 5, 7, 6, 2 }, "left"));
@@ -14,136 +17,80 @@ public class KakaoKeypad {
     }
 
     public String solution(int[] numbers, String hand) {
-
+        
         String answer = "";
-
-        int prevL = 10;
-        int prevR = 10;
-        int disL = 1;
-        int disR = 1;
-
-        for (int item : numbers) {
-
-            if (item % 3 == 1) {
+        // 맨해튼 거리 구하기 
+        // |(x2 - x1) + (y2 - y1)|
+        ArrayList<Point> pointAL = new ArrayList<>();
+        
+        pointAL.add(new Point(0, 3, 1));
+        pointAL.add(new Point(1, 0, 0));
+        pointAL.add(new Point(2, 0, 1));
+        pointAL.add(new Point(3, 0, 2));
+        pointAL.add(new Point(4, 1, 0));
+        pointAL.add(new Point(5, 1, 1));
+        pointAL.add(new Point(6, 1, 2));
+        pointAL.add(new Point(7, 2, 0));
+        pointAL.add(new Point(8, 2, 1));
+        pointAL.add(new Point(9, 2, 2));
+        pointAL.add(new Point(-1, 3, 0));
+        pointAL.add(new Point(-2, 3, 2));
+        
+        Point currPoint = null;
+        Point leftPoint = pointAL.get(10);
+        Point rightPoint = pointAL.get(11);
+        
+        for (int number: numbers) {
+            
+            currPoint = pointAL.get(number);
+            
+            switch(number){
+            case 1: case 4: case 7:
+                leftPoint = pointAL.get(number);
                 answer += "L";
-                prevL = item;
-                continue;
-            } else if (item != 0 && item % 3 == 0) {
+                break;
+            case 3: case 6: case 9:
+                rightPoint = pointAL.get(number);
                 answer += "R";
-                prevR = item;
-                continue;
-            } else if (item == 0) {
-                switch (prevL) {
-                case 1:
-                    disL += 1;
-                case 4: case 2:
-                    disL += 1;
-                case 7: case 5:
-                    disL += 1;
-                    break;
-                case 10:
-                    disL = 10;
-                    break;
-                case 8:
-                    break;
-                }
-                switch (prevR) {
-                case 3:
-                    disR += 1;
-                case 6: case 2:
-                    disR += 1;
-                case 9: case 5:
-                    disR += 1;
-                    break;
-                case 10:
-                    disL = 10;
-                    break;
-                case 8:
-                    break;
-                }
-            } else {
-                switch (item) {
-                case 2:
-                    switch (prevL) {
-                    case 0: case 7:
-                        disL += 1;
-                    case 4:
-                    case 8:
-                        disL += 1;
+                break;
+            default:
+                int leftD = Math.abs(currPoint.x - leftPoint.x) + Math.abs(currPoint.y - leftPoint.y);
+                int rightD = Math.abs(currPoint.x - rightPoint.x) + Math.abs(currPoint.y - rightPoint.y);
+                
+                if (leftD == rightD) {
+                    char checkChar = hand.charAt(0);
+                    switch (checkChar) {
+                    case 'l':
+                        answer += "L";
+                        leftPoint = pointAL.get(number);
                         break;
-                    case 10:
-                        disL = 10;
-                    }
-                    switch (prevR) {
-                    case 9: case 0:
-                        disR += 1;
-                    case 6:
-                    case 8:
-                        disR += 1;
+                    case 'r':
+                        answer += "R";
+                        rightPoint = pointAL.get(number);
                         break;
-                    case 10:
-                        disR = 10;
                     }
-                    break;
-                case 5:
-                    switch (prevL) {
-                    case 1:
-                    case 7:
-                    case 0:
-                        disL += 1;
-                        break;
-                    case 10:
-                        disL = 10;
-                    }
-                    switch (prevR) {
-                    case 3:
-                    case 9:
-                    case 0:
-                        disR += 1;
-                        break;
-                    case 10:
-                        disL = 10;
-                    }
-                    break;
-                case 8:
-                    switch (prevL) {
-                    case 1: case 2:
-                        disL += 1;
-                    case 4: case 5:
-                        disL += 1;
-                        break;
-                    case 10:
-                        disL = 10;
-                    }
-                    switch (prevR) {
-                    case 3:
-                        disR += 1;
-                    case 6:
-                        disR += 1;
-                        break;
-                    case 10:
-                        disL = 10;
-                    }
-                    break;
-                }
-            }
-            if (disL < disR) {
-                answer += "L";
-                prevL = item;
-            } else if (disL > disR) {
-                answer += "R";
-                prevR = item;
-            } else {
-                answer += hand.substring(0, 1).toUpperCase();
-                if (hand.substring(0, 1).equals("l")) {
-                    prevL = item;
+                } else if (leftD < rightD) {
+                    answer += "L";
+                    leftPoint = pointAL.get(number);
                 } else {
-                    prevR = item;
+                    answer += "R";
+                    rightPoint = pointAL.get(number);
                 }
+                break;
             }
-            disL = 1;
-            disR = 1;
         }
         return answer;
+    }
+    
+    public class Point {
+        int value;
+        int x;
+        int y;
+        
+        public Point(int value, int x, int y) {
+            this.value = value;
+            this.x = x;
+            this.y = y;
+        }
     }
 }
