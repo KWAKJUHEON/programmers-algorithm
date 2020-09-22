1cd919ea4a92fdd766c45347532cc273d908092c
kwakjuheon <juhun2005@naver.com>
2020-09-22 14:48:09 +0900

    주석 추가

--- src/test/algorithm/programmers/solution/level1/KakaoKeypad.java
+++ src/test/algorithm/programmers/solution/level1/KakaoKeypad.java
@@ -2,25 +2,32 @@ package test.algorithm.programmers.solution.level1;
 
 import java.util.ArrayList;
 
+/**
+ * 키패드 거리 구하기 
+ * @author kwakjuheon
+ * @since Sep 22, 2020
+ *
+ */
 public class KakaoKeypad {
-    // 1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5
-    // L R L L L R L L R R L
 
     public void run() {
-        System.out.println("************************"); // LRLLLRLLRRL
-        System.out.println("1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5");
         System.out.println(solution(new int[] { 1, 3, 4, 5, 8, 2, 1, 4, 5, 9, 5 }, "right"));
-        System.out.println("************************"); // LRLLRRLLLRR
         System.out.println(solution(new int[] { 7, 0, 8, 2, 8, 3, 1, 5, 7, 6, 2 }, "left"));
-        System.out.println("************************"); // LLRLLRLLRL
         System.out.println(solution(new int[] { 1, 2, 3, 4, 5, 6, 7, 8, 9, 0 }, "right"));
     }
 
+    /**
+     * 키패드 거리 구하기 함수 
+     * @author kwakjuheon
+     * @since Sep 22, 2020
+     * @param numbers 순서대로 눌러야 할 번호 목록 
+     * @param hand 주로 사용하는 손 
+     * @return 키패드별로 사용한 손의 리스 
+     */
     public String solution(int[] numbers, String hand) {
         
         String answer = "";
-        // 맨해튼 거리 구하기 
-        // |(x2 - x1) + (y2 - y1)|
+        // 맨해튼 거리 구하기 |(x2 - x1) + (y2 - y1)|
         ArrayList<Point> pointAL = new ArrayList<>();
         
         pointAL.add(new Point(0, 3, 1));
@@ -82,9 +89,19 @@ public class KakaoKeypad {
         return answer;
     }
     
+    
+    /**
+     * 키패드를 좌표로 나타냈을 경우의 위치 정
+     * @author kwakjuheon
+     * @since Sep 22, 2020
+     *
+     */
     public class Point {
+        // 키패드의 번호 값 
         int value;
+        // x 좌표 
         int x;
+        // y 좌표 
         int y;
         
         public Point(int value, int x, int y) {
